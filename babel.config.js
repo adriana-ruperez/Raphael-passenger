3834235889e67323ea5be40b72f834bfb0e2b3a2
Adriana Rupérez <adriana.rupereza@gmail.com>
2026-06-10 19:28:43 +0200

    first ios version

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = function babelConfig(api) {
   api.cache(true);
 
   return {
-    // Keep the Babel preset aligned with the installed Expo SDK.
-    presets: [require.resolve('expo/node_modules/babel-preset-expo')],
+    presets: ['babel-preset-expo'],
   };
 };
